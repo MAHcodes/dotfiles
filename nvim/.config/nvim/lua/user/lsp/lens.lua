4b1282f654801a7db365a49d1fdde32c1f7b0f3d
mahcodes <mhmdali102@bk.ru>
2023-12-28 13:37:34 +0200

    this is my quickfix branch and i will use to do my quickfixes

--- nvim/.config/nvim/lua/user/lsp/lens.lua
+++ nvim/.config/nvim/lua/user/lsp/lens.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 lens.setup({
-  enable = true,
+  enable = false,
   include_declaration = false, -- Reference include declaration
   sections = {                 -- Enable / Disable specific request
     definition = true,
