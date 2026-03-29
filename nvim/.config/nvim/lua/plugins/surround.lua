c03a1d3ec22a1bdfcb06f1b393e257d0214a149c
mahcodes <mhmdali102@bk.ru>
2026-03-29 21:49:22 +0300

    fix tpyo

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,19 +1,4 @@
 return {
 	"kylechui/nvim-surround",
-	event = "InsertEnter",
-	keys = { "s" },
-	opts = {
-		keymaps = {
-			insert = "<C-g>s",
-			insert_line = "<C-g>S",
-			normal = "s",
-			normal_cur = "ss",
-			normal_line = "S",
-			normal_cur_line = "SS",
-			visual = "s",
-			visual_line = "<Nop>",
-			delete = "sd",
-			change = "sc",
-		},
-	},
+	event = "VeryLazy",
 }
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,41 +1,11 @@
-# prompt and verbose
-alias cp='cp -iv'
-alias mv='mv -iv'
-alias rm='rm -iv'
-
-# better alternatives
-alias cat="bat"
-
 # neovim
 alias n="nvim"
 alias kvim="NVIM_APPNAME=kvim command nvim"
 alias lvim="NVIM_APPNAME=lvim command nvim"
 
-# git
-alias gs="git status"
-alias gd="git diff"
-alias ga="git add"
-alias gc="git commit -m"
-alias gp="git pull"
-alias gP="git push"
-
-# mpv
-alias ytmp4="mpv --ytdl --ytdl-format='bestvideo[height=480]+bestaudio' --tls-verify"
-alias ytmp3="mpv --ytdl --ytdl-format='bestaudio' --tls-verify --no-video"
-
-# yt-dlp
-alias dlpmp3="yt-dlp -f bestaudio -x --audio-format mp3 --embed-thumbnail --add-metadata -o '%(title)s.mp3' --"
-
-# sys
-alias uu="sudo apt update && sudo apt upgrade -y"
-alias nu="sudo nala upgrade"
-
 # misc
 alias slmake="rm -rf config.h; sudo make clean install; make clean"
 alias nvd="__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia"
-alias mochify="lutgen apply -p catppuccin-mocha"
 alias ip_wlp0s20f3="ip -brief addr | awk '/wlp0s20f3/{print \$3}'"
 alias twtui="taskwarrior-tui"
-
-# exit
 alias :q="exit"
